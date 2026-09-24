91dc6d4a226348821c963c13adbe4d86cb917d57
LAJO, Josef Lahmer <josef.lahmer@gugler.at>
2026-09-25 00:20:33 +0200

    0.0.98 envvar handling

--- RelaxedIT/RelaxedIT.psm1
+++ RelaxedIT/RelaxedIT.psm1
@@ -1,7 +1,7 @@
-
+﻿
 function Test-RelaxedIT
 {
-    $ver = "0.0.97"
+    $ver = "0.0.98"
     write-host (Get-ColorText -text "[Test] ""RelaxedIT.module"" - optimized for pwsh7 v: $ver :-)")
     return $ver
 }
